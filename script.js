bc60c58761f53828f28a897fb45f03546f4d8bde
Andrea Macias <andreamacrz@gmail.com>
2024-03-15 15:47:05 -0600

    tile class updated, it maps to the x,y coordinates)

--- script.js
+++ script.js
@@ -13,6 +13,28 @@
         1024: '#d90000',
         2048: '#c40000'   // stronger pink
     };
+
+    class Tile {
+        constructor(x, y, num) {
+
+            this.tileElem = document.createElement("div");
+            this.tileElem.classList.add("tile");
+            grid.append(this.tileElem);
+
+            this.num = num;
+            this.tileElem.innerText = this.num;
+            this.tileElem.style.backgroundColor = COLORS[num];
+
+            this.x = x;
+            this.tileElem.style.setProperty("--x", x);
+
+            this.y = y;
+            this.tileElem.style.setProperty("--y", y);
+
+        }
+    }
+
+ 
     
 
 	/*----- state variables -----*/
@@ -21,8 +43,16 @@
     let availableRowIdx;
     let availableColIdx;
     let winner;
+
 	/*----- cached elements  -----*/
-    grid = Array.from(document.querySelector("#grid > div"));
+    // grid = Array.from(document.querySelector("#grid > div"));
+    grid = document.getElementById("grid");
+
+    // test the tiles:
+    // let tile = new Tile(0, 0, 8)
+    // console.log(tile);
+
+
 
 	/*----- event listeners -----*/
 
@@ -57,6 +87,7 @@ function moveLeft(arr){ // this function is functional (only on the console for 
     for (let i = digits.length - 1; i >= 0; i--){
         if (digits[i] === digits[i - 1]){
             digits[i] = digits[i] * 2;
+            //add animation to the digits[i];
             digits.splice(i - 1, 1); // delete number before de sum
             zeros.push(0); // add one more zero to the zero's array to account for the deleted digit
         }
@@ -95,7 +126,8 @@ function populateAvailableIdx(board){
 function addNum(){ // addNum will be added on all the keys functions
     let rowIdx = availableRowIdx[Math.floor(Math.random() * availableColIdx.length + 1)];
     let colIdx = availableColIdx[Math.floor(Math.random() * availableColIdx.length + 1)];
-    board[rowIdx][colIdx] = Math.random() > 0.5 ? 2 : 4;;
+    board[rowIdx][colIdx] = Math.random() > 0.5 ? 2 : 4;
+    // add animation
 }
 
 function checkForWinner(){
@@ -112,8 +144,13 @@ function renderBoard(board){
     // change .style.bacgrounColor depending on the number inside the div using object COLORS. 
 }
 
+function renderMessages(){
+
+}
+
 function render(){
     renderBoard();
 }
+
 
 /*--- if time permits, creat a Score Board---*/
